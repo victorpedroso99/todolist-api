fd121b24b7288320bf34848ba2fe25f4f62970f6
victorpedroso <victor1pedroso@gmail.com>
2024-09-28 17:08:11 -0300

    Criação do modelo e contexto do bd

---
+++ todolist-api/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace todolist_api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ todolist-api/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace todolist_api.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
